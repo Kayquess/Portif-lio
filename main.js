3b3a2fbadf9a009fae218d6cccafbd31dafebab6
Kayque Silva <pkayque09@gmail.com>
2024-05-28 20:02:05 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+function typeWriter(e) {
+  const textoArray = e.innerHTML.split('')
+  e.innerHTML = ''
+  textoArray.forEach((letra, i) => {
+         setTimeout(() => e.innerHTML += letra, 75 * i)});
+}
+const texto = document.querySelector('h1')
+typeWriter(texto)
+
+function typeWriter(e) {
+  const textoArray = e.innerHTML.split('')
+  e.innerHTML = ''
+  textoArray.forEach((letra, i) => {
+         setTimeout(() => e.innerHTML += letra, 75 * i)});
+}
+const test = document.querySelector('p')
+typeWriter(test)
+
+function typeWriter(e) {
+  const textoArray = e.innerHTML.split('')
+  e.innerHTML = ''
+  textoArray.forEach((letra, i) => {
+         setTimeout(() => e.innerHTML += letra, 75 * i)});
+}
+const meio = document.querySelector('.texto')
+typeWriter(meio)
+
+function typeWriter(e) {
+  const textoArray = e.innerHTML.split('')
+  e.innerHTML = ''
+  textoArray.forEach((letra, i) => {
+         setTimeout(() => e.innerHTML += letra, 75 * i)});
+}
+const final = document.querySelector('.final')
+typeWriter(final)
+
+function typeWriter(e) {
+  const textoArray = e.innerHTML.split('')
+  e.innerHTML = ''
+  textoArray.forEach((letra, i) => {
+         setTimeout(() => e.innerHTML += letra, 75 * i)});
+}
+const projeto = document.querySelector('.nome')
+typeWriter(projeto)
+
+function typeWriter(e) {
+  const textoArray = e.innerHTML.split('')
+  e.innerHTML = ''
+  textoArray.forEach((letra, i) => {
+         setTimeout(() => e.innerHTML += letra, 75 * i)});
+}
+const prime = document.querySelector('.primeiro')
+typeWriter(prime)
+
